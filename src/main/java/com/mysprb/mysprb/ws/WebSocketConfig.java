7d9418e20a8cb7e0f82267b7a111ef7f122226d4
80493 <804937921@qq.com>
2020-07-19 11:43:44 +0800

    配置文件编码问题

--- src/main/java/com/mysprb/mysprb/ws/WebSocketConfig.java
+++ src/main/java/com/mysprb/mysprb/ws/WebSocketConfig.java
@@ -7,8 +7,12 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 /**
  * Created by Administrator on 2019/1/4.
  */
+/**
+ * @Configuration指名当前类为配置类，代替之前的spring配置文件
+ */
 @Configuration
 public class WebSocketConfig {
+    //将方法的返回值添加到容器中：容器中这个组件的默认id为方法名
     @Bean
     public ServerEndpointExporter serverEndpointExporter(){
         return new ServerEndpointExporter();
